f4d216958cfc9afa7730179bef851e723d69ca2b
Robbie Mackay <rm@robbiemackay.com>
2012-05-04 18:45:35 +1200

    Fix up 401 message

--- i18n/en_US/restapi_error.php
+++ i18n/en_US/restapi_error.php
@@ -4,6 +4,6 @@ $lang = array
 (
 	'error_404' => 'The resource you requested, %s, could not be found.',
 	'error_501' => 'The action you have requested, %s,  is not implemented.',
-	'error_401' => 'You are you authorized to complete the request, %s.',
+	'error_401' => 'You are not authorized to complete the request, %s.',
 	'error_405' => 'Your request, %s, is not allowed.',
 );
